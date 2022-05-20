ba5ae963604c364d9a0e2a19ea0865aefa04ca76
Jonathan Leverenz <jonlev03@gmail.com>
2022-05-20 14:59:01 -0400

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import Styles from "./Footer.module.css";
+export default function Footer() {
+  return (
+    <footer className={Styles.footer}>
+      <p>&#169; Jonathan Leverenz</p>
+    </footer>
+  );
+}
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,6 +1,7 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Styles from "./Landing.module.css";
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
 export default function Landing() {
   return (
     <>
@@ -8,6 +9,7 @@ export default function Landing() {
       <main className={Styles.main}>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
