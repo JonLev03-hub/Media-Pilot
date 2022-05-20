f0a65dbc25cd0555ca5838333290101293df1b01
Jonathan Leverenz <jonlev03@gmail.com>
2022-05-19 23:44:55 -0400

    added another section and updated button code to fit new styles

--- src/pages/Landing/About.js
+++ src/pages/Landing/About.js
@@ -66,20 +66,31 @@ export default function About() {
         <div className={Styles.containerHalfImg}></div>
       </section>
       <section className={Styles.section}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          className={Styles.sectionBottomWave}
+        >
+          <path
+            fill="#FFFFFF"
+            fill-opacity="1"
+            d="M0,192L80,213.3C160,235,320,277,480,261.3C640,245,800,171,960,149.3C1120,128,1280,160,1360,176L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
         <div className={Styles.containerHalfImg}></div>
         <div className={Styles.containerHalf}>
           <h3 className={Styles.h3}>
-            Media Management
+            Reliable Analytics for
             <br />
-            made Easy
+            your business
           </h3>
           <p className={Styles.p}>
-            We stride to help businesses grow their online presence with the
-            click of a button. Providing scheduled posting, post suggestions,
-            and performance enhancing analytics is our job!
+            As your business grows your media will need to grow with it. That's
+            why we provide insights to what posts are making your business
+            known.
           </p>
           <Link className={Styles.button} to="/">
-            Explore Plans
+            See what we offer
           </Link>
         </div>
       </section>
